b3119f39dd728f4b40b14905b5d19e6fb28c0f77
lexavzie <lexavzie@ukr.net>
2015-12-07 20:52:45 +0200

    код на C#

---
+++ raduga/raduga/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: vzie
+ * Date: 16.11.2015
+ * Time: 22:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace raduga
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.ForegroundColor=ConsoleColor.Blue;
+			Console.SetCursorPosition (0,0);
+			Console.WriteLine("Василько");
+			
+			Console.ForegroundColor=ConsoleColor.Blue;
+			Console.SetCursorPosition (10,4);
+			Console.WriteLine("Василько");
+			
+			Console.ForegroundColor=ConsoleColor.Blue;
+			Console.SetCursorPosition (20,8);
+			Console.WriteLine("Василько");
+			
+			Console.ForegroundColor=ConsoleColor.Blue;
+			Console.SetCursorPosition (37,12);
+			Console.WriteLine("КРАЩИЙ");
+			
+			Console.ForegroundColor=ConsoleColor.Blue;
+			Console.SetCursorPosition (50,16);
+			Console.WriteLine("Василько");
+			
+			Console.ForegroundColor=ConsoleColor.Blue;
+			Console.SetCursorPosition (60,20);
+			Console.WriteLine("Василько");
+			
+			Console.ForegroundColor=ConsoleColor.Blue;
+			Console.SetCursorPosition (70,23);
+			Console.WriteLine("Василько");
+			
+			
+			Console.ReadKey(true);
+		}
+	}
+}
